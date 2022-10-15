a694f2b54b50bbc1361cadd5b70f61e3c8535349
DenisaRainova <Dendous@email.cz>
2022-10-15 14:38:29 +0200

    HTML CSS style + Humidity and wind

--- src/apps.js
+++ src/apps.js
@@ -3,10 +3,17 @@ function displayTemperature(response){
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElemenet = document.querySelector("#wind");
+    let feelsElement = document.querySelector("#feels");
+
 
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElemenet.innerHTML = Math.round(response.data.wind.speed);
+    feelsElement.innerHTML = Math.round(response.data.main.feels_like);
 }
 
 let apiKey = "b884720d8f959cc5aa66ad10c2546f56";
