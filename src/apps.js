2928904b153cae32f8291381d5daa8c0e1e2bed8
DenisaRainova <Dendous@email.cz>
2022-10-15 14:09:58 +0200

    city and description is working

--- src/apps.js
+++ src/apps.js
@@ -1,7 +1,12 @@
 function displayTemperature(response){
     console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "b884720d8f959cc5aa66ad10c2546f56";
