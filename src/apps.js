4ca564134ebcc9617c2d7a5c932de2bcfb7c6f93
DenisaRainova <Dendous@email.cz>
2022-10-15 16:01:10 +0200

    Celsius is working

--- src/apps.js
+++ src/apps.js
@@ -89,9 +89,18 @@ function displayFahrenheitTemperature(event){
     let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
     temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
   }
+
+function displayCelsiusTemperature(event){
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  }
   
 let fahrenheitLink = document.querySelector("#f-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#c-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
 
